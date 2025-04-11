23bc016a584aba1696c915112c282263ecbe9ade
Ryan Bunney <myxie@users.noreply.github.com>
2025-04-11 16:13:34 +0800

    Revert "Add pulsar workflows to the observation plan generator."

--- simulator_demonstration_experiments/topsim_test_simulations/playground_scalability_new.py
+++ simulator_demonstration_experiments/topsim_test_simulations/playground_scalability_new.py
@@ -66,13 +66,10 @@ from topsim.utils.experiment import Experiment
 # TODO Fix up all the combinations here with slurm and directories/paths
 LOGGER.debug("Using method: %s", args.method)
 if args.method == 'batch':
-    e = Experiment(cfg_paths, [("batch", "batch")], output=experiment_path.parent / "results",
-                   sched_args={"ignore_ingest": False, "use_workflow_dop": True},
-                   slurm=args.slurm)
-
+    e = Experiment(cfg_paths, [("batch", "batch")], output=experiment_path / "results")
 elif args.method == 'static':
     e = Experiment(cfg_paths, [("static", "dynamic_plan")], output=experiment_path.parent / "results",
-                   sched_args={"ignore_ingest": False, "use_workflow_dop": True},
+                   sched_args={"ignore_ingest": False, "use_workflow_dop": False},
                    slurm=args.slurm)
 else:
     e = Experiment(
